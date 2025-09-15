2e0776d7961ea1e8a7a4b4d13096389e092c5d38
Kangnets <kangnets88@gmail.com>
2025-09-15 17:16:06 +0900

    add: info-list 페이지 추가

---
+++ src/app/info-list/page.tsx
@@ -0,0 +1,5 @@
+export default function InfoListPage() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20"></div>
+  );
+}
