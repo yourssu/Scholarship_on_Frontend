a45c893c839216e9af18edef062823b8d1ac9750
Kangnets <kangnets88@gmail.com>
2025-09-15 17:24:44 +0900

    feat: 임시 테스트 코드 추가

--- src/app/info-list/page.tsx
+++ src/app/info-list/page.tsx
@@ -1,5 +1,7 @@
 export default function InfoListPage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20"></div>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      test
+    </div>
   );
 }
