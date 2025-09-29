58abdce01971a30a1a0f22da5792a85883dc3130
Kangnets <kangnets88@gmail.com>
2025-09-29 21:31:22 +0900

    feat : info-modal(장학 정보 모달 페이지) 기본 컴포넌트 개발

--- src/app/info-modal/page.tsx
+++ src/app/info-modal/page.tsx
@@ -1,7 +1,22 @@
+import { DetailDescription } from './_components/DetailDescription';
+import { ShortDescription } from './_components/ScholarshipName';
+
 export default function InfoModalPage() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      
+      <div>{/*헤더*/}</div>
+      <div className="flex flex-col gap-10">
+        <ShortDescription
+          name={'소득연계형 국가장학금'}
+          description={'소득연계형 국가장학금은 소득연계형 국가장학금입니다.'}
+        />
+        <DetailDescription
+          title={'주관기관'}
+          content={'한국 장학 재단'}
+          extra_title={'서류 제출'}
+          extra_content={'2025. 5. 23.(금) 9시 ~ 2025. 6. 30.(월) 18시'}
+        />
+      </div>
     </div>
   );
 }
